e5f479820343735c327a012c791f7cc75518a540
Graham <gmfreundlich@gmail.com>
2023-09-06 12:24:25 -0400

    allMovies get route works

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { createUser } from "../controllers/createUser";
+import { allMovies } from "../controllers/allMovies";
 
 const router = Router();
 
 router.route("/").post(createUser);
+
+router.route("/movies").get(allMovies)
 
 export default router;
