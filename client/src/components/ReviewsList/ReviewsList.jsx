479d12de72aee138c4d246e8c6033f38af619844
Kurt Wehde <kurt.m.wehde@mykenzie.snhu.edu>
2023-09-19 08:59:19 -0600

    Styling

--- client/src/components/ReviewsList/ReviewsList.jsx
+++ client/src/components/ReviewsList/ReviewsList.jsx
@@ -9,36 +9,39 @@ import { useNavigate } from 'react-router-dom';
 const ReviewsList = ({ reviews }) => {
   const navigate = useNavigate()
     return (
-      <div className="reviews-section">
-        <h2>User Reviews</h2>
-        <div className="review-container">
-          {reviews.map((review) => (
-            <div className="review">
-            <div className="user-info">
-              <h4 className="user-name">User: {review.username}</h4>
-              <h3 className="review-star-rating">
-                ★ {review.Rating}/5
-              </h3>
+<div className="reviews-section">
+      <h2>User Reviews</h2>
+      <div className="review-container">
+        {reviews.length > 0 ? (
+          reviews.map((review) => (
+            <div className="review" key={review._id}>
+              <div className="user-info">
+                <h4 className="user-name">User: {review.username}</h4>
+                <h3 className="review-star-rating">
+                  ★ {review.Rating}/5
+                </h3>
+              </div>
+              {review.commentText ? (
+                <>
+                  <p>Comment: {review.commentText}</p>
+                  <div className="thumbs">
+                    <FontAwesomeIcon className="icon-up" icon={faThumbsUp} />
+                    <FontAwesomeIcon className="icon-down" icon={faThumbsDown} />
+                  </div>
+                </>
+              ) : (
+                <p>No Review</p>
+              )}
+              <button className="reply-button" onClick={() => navigate(`/reviewDetail/${review._id}`)}>Reply</button>
             </div>
-            {review.commentText ? (
-              <>
-                <p>Comment: {review.commentText}</p>
-                <div className="thumbs">
-                  <FontAwesomeIcon className="icon-up" icon={faThumbsUp} />
-                  <FontAwesomeIcon className="icon-down" icon={faThumbsDown} />
-                </div>
-              </>
-            ) : (
-              <p>No Review</p>
-            )
-            }
-            <button className="reply-button" onClick={()=>navigate(`/reviewDetail/${review._id}`)}>Reply</button>
-          </div>          
-          ))}
-        </div>
+          ))
+        ) : (
+          <p>No user reviews yet. Start the conversation!</p>
+        )}
       </div>
-    );
-  };
+    </div>
+  );
+};
   
 
 export default ReviewsList;
